4a116b0e2f613562e987c5e61bcc43b55dce0581
Inara <inarasalih2015@gmail.com>
2021-01-29 11:43:33 -0600

    LogOut functionality have some changes

--- src/Sprint1/LogOutFunctionality.java
+++ src/Sprint1/LogOutFunctionality.java
@@ -7,6 +7,12 @@ public class LogOutFunctionality {
         then will add one more branch
         and will add some changes
 
+so now, working under branch2
+and will not add any changes other than this place
+cause, seems like
+conflict can be happen if under the same branch
+I`ll make changes in different place
+should check it
          */
     }
 }
