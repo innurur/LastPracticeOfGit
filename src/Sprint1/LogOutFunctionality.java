2d9c1ffeecc3ea7e67806f1677e4184de1cda5ea
Inara <inarasalih2015@gmail.com>
2021-01-29 11:41:30 -0600

    LogOut functionality is created

---
+++ src/Sprint1/LogOutFunctionality.java
@@ -0,0 +1,12 @@
+package Sprint1;
+
+public class LogOutFunctionality {
+    public static void main(String[] args) {
+        /*
+        so this functionality is created in master branch
+        then will add one more branch
+        and will add some changes
+
+         */
+    }
+}
