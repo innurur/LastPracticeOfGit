41fb71f6193d95e6800b0d8b31e18f23a8793383
Inara <inarasalih2015@gmail.com>
2021-01-29 11:37:28 -0600

    LogIn functionality started

--- src/Sprint1/LogInFunctionality.java
+++ src/Sprint1/LogInFunctionality.java
@@ -1,2 +1,11 @@
-package Sprint1;public class LogInFunctionality {
+package Sprint1;
+
+public class LogInFunctionality {
+    public static void main(String[] args) {
+        String username = "uhhfh56rg";
+        String passord = "hsfhg655";
+        // driver.login;
+
+    }
+
 }
